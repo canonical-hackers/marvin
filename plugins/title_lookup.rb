109f19953c7c8e223491e03450507a10603bdc35
Brian Haberer <bhaberer@gmail.com>
2012-04-21 13:25:09 -0700

    Cinch with working title lookup and UD plugins

---
+++ plugins/title_lookup.rb
@@ -0,0 +1,22 @@
+class TitleLookup
+  include Cinch::Plugin
+
+  listen_to :channel
+
+  def listen(m)
+    urls = URI.extract(m.message, ["http", "https"])
+    urls.each do |url| 
+      m.reply( "SHORTURL .:.  #{ get_title(url) || 'Untitled'}" )
+    end
+  end
+
+  private 
+
+  def get_title(url)
+    # Make sure the URL is legit
+    url = URI::extract(url, ["http", "https"]).first
+
+    # Grab the element
+    return Nokogiri::HTML(open(url)).css('title').first.content
+  end
+end
---
+++ plugins/urban_dictionary.rb
@@ -0,0 +1,25 @@
+class UrbanDictionary
+  include Cinch::Plugin
+
+  match /ud (.*)/
+
+  def execute(m, term)
+    m.reply( "Urban Dictionary .:. #{term}: #{get_def(term) || 'Definition not found'}")
+  end
+ 
+  private 
+
+  def get_def(term)
+    # URI Encode
+    term = URI.escape(term, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    
+    url = "http://www.urbandictionary.com/define.php?term=#{term}"
+    
+    # Make sure the URL is legit
+    url = URI::extract(url, ["http", "https"]).first
+
+    # Grab the element
+    return Nokogiri::HTML(open(url)).css('.definition').first.content
+  end
+end
+
