26459e621a2cc547a5a06b6a897ef8f85f147956
Brian Haberer <bhaberer@gmail.com>
2012-04-25 14:15:06 -0700

    removing storage and AR crap

--- bot.rb
+++ bot.rb
@@ -2,10 +2,6 @@ require "rubygems"
 require "bundler/setup"
 require 'yaml'
 require 'cinch'
-require 'cinch/storage/yaml'
-#require 'active_record'
-# dbconfig =     YAML::load(File.open('config/database.yml'))
-# ActiveRecord::Base.establish_connection(dbconfig)
 
 # Define an array that we can do help stuff, might be a better way to do this.
 $commands = Hash.new('Command not found')
@@ -29,10 +25,6 @@ Dir[File.join('.', 'plugins', '*.rb')].each { |file| require file }
     c.max_messages = 1
     c.plugins.prefix   = '.'
     c.plugins.plugins = config['plugins'].map { |plugin| Kernel.const_get(plugin) }
-    # Storage seems to be half baked, commenting this out till it's working.
-    c.storage.backend = Cinch::Storage::YAML
-    #c.storage.basedir = "./db/"
-    #c.storage.autosave = true
   end
 
   on :channel, /^.help/ do |m|
