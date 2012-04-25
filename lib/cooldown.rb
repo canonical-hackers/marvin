4ab22d564e424ef4b89fedfa755e058ab21b00b1
Brian Haberer <bhaberer@gmail.com>
2012-04-24 21:10:37 -0700

    Fixing thread safeness in cooldown

--- lib/cooldown.rb
+++ lib/cooldown.rb
@@ -4,27 +4,24 @@ module Cinch
       def cooldown
         if !$cooldown.nil? && $cooldown.key?(:timer)
           hook(:pre, :for => [:match], :method => lambda {|m| cooldown_finished?(m)})
-          hook(:post, :for => [:match], :method => lambda {|m| reset_cooldown(m) }) 
         end
       end
     end
 
     def cooldown_finished?(m)
-      if $cooldown.key?(:time)
-        if $cooldown[:timer] < (Time.now - $cooldown[:time]).floor
-          $cooldown[:time] = Time.now
-          return true
+      synchronize(:cooldown) do 
+        if $cooldown.key?(:time)
+          if $cooldown[:timer] < (Time.now - $cooldown[:time]).floor
+            $cooldown[:time] = Time.now
+            true
+          else 
+            false
+          end
         else 
-          debug "Command dropped due to cooldown: #{m.message} #{$cooldown[:timer] - (Time.now - $cooldown[:time])} seconds left before commands are accepted."
-          return false 
+          $cooldown[:time] = Time.now
+          true
         end
-      else 
-        true
       end
-    end
-
-    def reset_cooldown(m)
-      debug "Resetting Cooldown"
     end
   end
 end
