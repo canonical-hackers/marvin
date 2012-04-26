5ff73b4a9d223917148d6eeea869961eca09fc5d
Brian Haberer <bhaberer@gmail.com>
2012-04-26 13:32:39 -0700

    Cleanup of bot file

--- bot.rb
+++ bot.rb
@@ -3,14 +3,13 @@ require "bundler/setup"
 require 'yaml'
 require 'cinch'
 
-# Define an array that we can do help stuff, might be a better way to do this.
-#$commands = Hash.new('Command not found')
+# Load the bot config 
 config = YAML::load(File.open("config/#{ARGV[0] || 'bot'}.yml"))
 
 # Setup the cooldown if one is configured
 $cooldown = { :config => config['cooldowns'] } if config['cooldowns']
 
-# Load misc files
+# Load Libs
 Dir[File.join('.', 'lib', '*.rb')].each { |file| require file }
 
 # Load Plugins
@@ -26,15 +25,6 @@ Dir[File.join('.', 'plugins', '*.rb')].each { |file| require file }
     c.plugins.prefix   = '.'
     c.plugins.plugins = config['plugins'].map { |plugin| Kernel.const_get(plugin) }
   end
-
-  #on :channel, /^.help/ do |m|
-  #  command = m.message.match(/^.help (.*)/)[1] rescue nil
-  #  if command
-  #    m.reply $commands[command], true
-  #  else
-  #    m.reply "The following help topics are available: #{$commands.keys.join(', ')}."
-  #  end
-  #end
 end
 
 @bot.start
