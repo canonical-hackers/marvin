1ad80d89937b7cd4a33879e5298f74ee7ddc02fb
Brian Haberer <bhaberer@gmail.com>
2012-04-26 17:44:01 -0700

    Add URL expander, tweak config on shortener.

--- lib/common.rb
+++ lib/common.rb
@@ -1,24 +1,32 @@
 require 'ruby-bitly'
 
+def expand(url)
+  @bitlyconfig = YAML::load(File.open('config/bitly.yml'))
+  if url.match(/bit\.ly/) 
+    return Bitly.expand(url, @bitlyconfig['username'], @bitlyconfig['apikey']).long_url
+  else 
+    return url 
+  end
+end
+
 def shorten(url)
   # Let's not shorten urls that don't really need it
 
   # Use the youtube shortener matcher if it's for YT
   if yt_id = url.match(/http:\/\/w{3}?\.?youtube\.com.+(\S{11})/) 
     return "http://youtu.be/#{yt_id[1]}"
-  # Short Enough
-  elsif url.length < 25
-    return url
-  # Already Shortened
-  elsif url.match(/^http:\/\/bit\.ly/) || 
+        # Short Enough
+  elsif url.length < 45 || 
+        # Already Shortened
+        url.match(/^http:\/\/bit\.ly/) || 
         url.match(/^http:\/\/tinyurl\.com/) || 
         url.match(/^http:\/\/t\.co/) || 
-        url.match(/^http:\/\/goo\.gl/) 
+        url.match(/^http:\/\/goo\.gl/)
     return url     
-  else
-    @bitlyconfig = YAML::load(File.open('config/bitly.yml'))
-    return Bitly.shorten(url, @bitlyconfig['username'], @bitlyconfig['apikey']).url
   end
+  
+  @bitlyconfig = YAML::load(File.open('config/bitly.yml'))
+  return Bitly.shorten(url, @bitlyconfig['username'], @bitlyconfig['apikey']).url
 end
 
 def truncate(text, length = 250) 
