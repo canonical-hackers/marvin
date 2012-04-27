6b904cd40f20fdaaee493dece17f00dd480e0d0c
Brian Haberer <bhaberer@gmail.com>
2012-04-26 17:45:08 -0700

    Add storage to Links, add reddit image title fetch

--- plugins/link_logger.rb
+++ plugins/link_logger.rb
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 class LinkLogger
-  class Link < Struct.new(:link, :nick, :title, :short_url, :time)
-    def to_s 
-      "#{short_url} ∴ #{title} (#{time.ago_in_words})"
-    end 
-  end
-
   include Cinch::Plugin
   require 'nokogiri'
   require 'open-uri'
@@ -16,26 +10,55 @@ class LinkLogger
 
   def initialize(*args)
     super
-    @history = Hash.new(0)
+    @storage = Storage.new('yaml/links.yaml') 
+    @storage.data[:history] ||= Hash.new
   end
 
   def execute(m)
+    top10 = @storage.data[:history][m.channel.name].values.sort {|a,b| b[:time] <=> a[:time] }
     m.user.send "Recent Links in #{m.channel}"
-    top10 = @history.values.sort {|a,b| b[:time] <=> a[:time] }
-    top10[0,10].each_with_index { |link, i| m.user.send "#{i + 1}. #{link}" }  
+    top10[0,10].each_with_index do |link, i|
+      if link[:title].nil? 
+        m.user.send "#{i + 1}. #{expand(link[:short_url])}"
+      else 
+        m.user.send "#{i + 1}. #{link[:short_url]} ∴ #{link[:title]}"  
+      end
+    end
   end
 
   def listen(m)
     urls = URI.extract(m.message, ["http", "https"])
     urls.each do |url|
-      if @history.key?(url)
-        m.reply("#{@history[url].short_url} ∴  #{@history[url].title}")
-        m.reply "That was already linked by #{@history[url].nick} #{@history[url].time.ago_in_words}.", true
+      @storage.data[:history][m.channel.name] ||= Hash.new 
+  
+      if @storage.data[:history][m.channel.name].key?(url)
+        link = @storage.data[:history][m.channel.name][url]
+        m.reply("#{link[:short_url]} ∴  #{link[:title]}") unless link[:title].nil?
+        if link[:count] == 1
+          m.reply "That was already linked by #{link[:nick]} #{link[:time].ago_in_words}.", true 
+        else 
+          m.reply "That was already linked #{link[:count]} times. " + 
+                  "#{link[:nick]} was the first to link it #{link[:time].ago_in_words}.", true
+        end
+        @storage.data[:history][m.channel.name][url][:count] += 1
       else   
         short_url = shorten(url)
-        title = get_title(url).gsub(/\n/, '') || 'Untitled'
-        m.reply("#{short_url} ∴  #{title}")
-        @history[url] = Link.new(url, m.user.nick, title, short_url, Time.now)
+        title = get_title(url)
+        
+        # Only spam the channel if you have a title and url.
+        m.reply("#{short_url} ∴  #{title}") if short_url && title 
+
+        @storage.data[:history][m.channel.name][url] = {:nick => m.user.nick, 
+                                                        :title => title || nil, 
+                                                        :count => 1,
+                                                        :short_url => short_url,
+                                                        :time => Time.now }
+      end
+    end
+
+    if urls 
+      synchronize(:save_links) do 
+        @storage.save   
       end
     end
   end
@@ -46,7 +69,18 @@ class LinkLogger
     # Make sure the URL is legit
     url = URI::extract(url, ["http", "https"]).first
 
-    # Grab the element
-    return Nokogiri::HTML(open(url)).css('title').first.content.strip.gsub(/\s+/, ' ')
+    if url.match(/\.jpg|gif|png$/)
+      if url.match(/^http:\/\/i\.imgur\.com/)
+        url = "http://imgur.com/#{url.match(/([A-Za-z0-9]{5})\.jpg|gif|png$/)[1]}"
+      else 
+        return "Image: #{url.match(/\/([^\/]+\.jpg|gif|png)$/)[1]}"
+      end
+    end
+   
+    debug url
+    
+    # Grab the element, return nothing if  the site doesn't have a title.
+    page = Nokogiri::HTML(open(url)).css('title')
+    return page.first.content.strip.gsub(/\s+/, ' ') unless page.empty?
   end
 end
