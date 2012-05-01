2b5b9b5a865b7a566d9f55a1c09ddea69cf7d986
Brian Haberer <bhaberer@gmail.com>
2012-05-01 16:51:56 -0700

    Refactor (with thanks to dominikh) plus adding support for atomic timers in multiple channels.

--- lib/cooldown.rb
+++ lib/cooldown.rb
@@ -2,28 +2,29 @@ module Cinch
   module Plugin
     module ClassMethods
       def cooldown
-        if !$cooldown.nil? && $cooldown.key?(:config)
+        if $cooldown && $cooldown[:config]
           hook(:pre, :for => [:match], :method => lambda {|m| cooldown_finished?(m)})
         end
       end
     end
 
     def cooldown_finished?(m)
-      synchronize(:cooldown) do 
-        if $cooldown[:config].key?(m.channel)
-          if $cooldown.key?(:time)  
-            if $cooldown[:config][m.channel] < (Time.now - $cooldown[:time]).floor
-              $cooldown[:time] = Time.now
-              return true
-            else 
-              debug "Cooldown: #{($cooldown[:config][m.channel] - (Time.now - $cooldown[:time])).floor}s"
-              return false
-            end
-          else 
-            $cooldown[:time] = Time.now
-            return true
-          end
+      synchronize(:cooldown) do
+        return true unless $cooldown[:config][m.channel]
+
+        unless $cooldown[m.channel] 
+          $cooldown[m.channel] = Time.now
+          return true
         end
+
+        cooldown = (Time.now - $cooldown[m.channel]).floor
+        if $cooldown[:config][m.channel] < cooldown
+          $cooldown[m.channel] = Time.now
+          return true
+        end
+
+        debug "Cooldown: #{cooldown[:config][m.channel] - cooldown}s"
+        return false
       end
     end
   end
