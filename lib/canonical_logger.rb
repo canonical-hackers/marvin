fae1ccadc76e92ce9aac8f78f9a2c151d08fa3da
Brian Haberer <bhaberer@gmail.com>
2012-05-07 14:32:39 -0700

    Adding a logger to allow us to log to the filesystem

---
+++ lib/canonical_logger.rb
@@ -0,0 +1,71 @@
+require "cinch/logger"
+module Cinch
+  class Logger
+    class CanonicalLogger < Cinch::Logger
+      # (see Logger#log)
+      def log(messages, event = :debug, level = event)
+        return unless will_log?(level)
+
+        @mutex.synchronize do
+          # Ensure that we're logging to the right file. 
+          Array(messages).each do |message|
+            next unless message.match(Regexp.new("PRIVMSG ##{@channel}"))           
+            message = format_general(message)
+            message = format_message(message, event)
+
+            next if message.nil?
+            sync_logfile
+            @output.puts message.encode("locale", {:invalid => :replace, :undef => :replace})
+          end
+        end
+      end
+      
+      def initialize(channel, bot)
+        @bot_name = bot
+        @channel  = channel
+        @output   = File.open(today_log(@channel), 'a')
+        @mutex    = Mutex.new
+        @level    = :debug
+      end
+
+      private
+
+      def today_log(channel)
+        "./logs/#{channel}_#{Time.now.strftime("%Y-%m-%d")}.log"
+      end
+
+      def format_incoming(message)
+        
+        nick = message.match(/^:(\S+)!/)[1] rescue 'NONICK'
+        msg  = message.match(/#\S+ :(.+)$/)[1] rescue 'NOMSG'
+        generic_format(nick, msg)
+      end
+
+      def format_outgoing(message)
+        msg = message.match(/#\S+ :(.+)$/)[1] rescue 'NOMSG'
+        generic_format(@bot_name, msg)
+      end
+
+      def generic_format(nick, m) 
+        message = '[' + Time.now.strftime("%Y-%m-%d %H:%M:%S") + '] '
+        # This seems gross, fix this with better rubies.
+        if m.codepoints.first == 1 && m.match(/ACTION/) 
+          message << " * #{nick} #{m.gsub(/^.?ACTION /, '')}"
+        else 
+          message << "<#{nick}> #{m}"
+        end
+        puts m.codepoints.first
+        puts m
+        puts message
+        return message
+      end
+
+      def sync_logfile
+        unless @output.path == today_log(@channel)
+          @output.close
+          @output = File.open(today_log(@channel), 'a')
+        end
+      end
+    end
+  end
+end
