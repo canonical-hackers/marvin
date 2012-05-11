ecb9239395ae66e94913d27fd0031c31c38b8b4b
Brian Haberer <bhaberer@gmail.com>
2012-05-10 17:27:33 -0700

    Few dbag bugs fixed

--- plugins/dickbag.rb
+++ plugins/dickbag.rb
@@ -8,7 +8,7 @@ class Dickbag
     
   def initialize(*args)
     super
-    @owner = {} 
+    @dickbag = {}
   end
 
   self.help = "Use .dickbag to get the bag, I don't really want to know why you want it so bad."
@@ -16,44 +16,60 @@ class Dickbag
 
   set(:prefix => '') 
 
-  match /^[!\.]dickbag$/, method: :steal
+  match /^[!\.]dickbag$/, method: :dickbag
   match /^[!\.]dickbag info/, method: :info    
 
   def listen(m)
+    @dickbag[:current] = {} unless @dickbag.key?(:current)
     
-    if m.user.nick == @owner[:nick] && m.action_message && m.action_message.match(/the\sbag\sof\sdicks/)
-      action = m.action_message.match(/(\S+)\sthe\sbag\sof\sdicks/)[1]
+    if m.user.nick == @dickbag[:current][:nick] && m.action_message && m.action_message.match(/bag of dicks|dickbag/)
+      action = m.action_message.match(/^(.*) dickbag/)[1]
       if action.match(/noms/)
-        m.channel.action "recoils in horror as #{@owner[:nick]} gobbles down the bag of dicks!"
-        @owner = {}
+        @dickbag[:last] = {:action => 'nom', :nick => @dickbag[:current][:nick]}  
+        @dickbag[:current] = Hash.new
       elsif action.match(/hides/)
-        m.channel.action "looks confused as #{@owner[:nick]} hides the bag of dicks in their pants."
+        @dickbag[:last] = {:action => 'hid', :nick => @dickbag[:current][:nick]}  
+        @dickbag[:current] = Hash.new 
       end  
     end
   end
 
-  def steal(m)
-    if @owner.key?(:nick)
-      if @owner[:nick] == m.user.nick
-        m.reply "you still have the bag of dicks, chill the fuck out.", true
+  def dickbag(m)
+    if @dickbag[:current].key?(:nick)
+      if @dickbag[:current][:nick] == m.user.nick
+        m.reply "you still have the bag of dicks. Chill the fuck out.", true
       else 
-        m.channel.action "reaches over to #{@owner[:nick]} takes the bag of dicks, and hands it to #{m.user.nick}"
-        @owner = {:nick => m.user.nick, :time => Time.now, :times_passed => @owner[:times_passed] + 1 } 
-      end 
-    else 
+        m.channel.action "reaches over to #{@dickbag[:current][:nick]} takes the bag of dicks and hands it to #{m.user.nick}"
+        @dickbag[:current] = {:nick => m.user.nick, :time => Time.now, 
+                              :times_passed => @dickbag[:current][:times_passed] + 1 } 
+      end   
+    elsif @dickbag.key?(:last)
+      if @dickbag[:last][:action] == 'nom'
+        m.channel.action "grabs a new bag of dicks for #{m.user.nick} since #{@dickbag[:last][:nick]} went all nomnomonom on the last one."
+        @dickbag[:current] = {:nick => m.user.nick, :time => Time.now, :times_passed => 0 }
+      elsif @dickbag[:last][:action] == 'hid'
+        m.channel.action "grabs a new bag of dicks for #{m.user.nick} since the last one seems to have vanished."
+        @dickbag[:current] = {:nick => m.user.nick, :time => Time.now, :times_passed => 0 }
+      end
+    else  
       m.channel.action "reaches down and grabs a new bag of dicks and hands it to #{m.user.nick}"
-      @owner = {:nick => m.user.nick, :time => Time.now, :times_passed => 0 } 
+      @dickbag[:current] = {:nick => m.user.nick, :time => Time.now, :times_passed => 0 }
     end
   end
 
   def info(m)
-    if @owner.key?(:nick)
-      message = "#{@owner[:nick]} is currently holding the bag of dicks, they stole it #{@owner[:time].ago_in_words}"
-      if @owner[:times_passed] > 0 
-        message += " and #{@owner[:times_passed]} other people have had their filthy hands all over them."
-      else 
-        message += '.'
+    if @dickbag.key?(:current)
+      message = "#{@dickbag[:current][:nick]} is currently holding the bag of dicks"
+
+      if @dickbag[:current].key?(:time)
+        message << ", they stole it #{@dickbag[:current][:time].ago_in_words}"
       end 
+
+      unless @dickbag[:current][:times_passed] == 0
+        message << " and #{@dickbag[:current][:times_passed]} other people have had their filthy hands all over them"
+      end
+
+      message << '.'
     else
       message = "no one seems to want my bag of dicks :("
     end
