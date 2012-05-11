bf5875e80ad63e7677840d0cd821c0e71e61811e
Brian Haberer <bhaberer@gmail.com>
2012-05-10 17:27:55 -0700

    Adding support to auto post images linked to a tumblr

--- bot.rb
+++ bot.rb
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'bundler/setup'
 require 'yaml'
+
 require 'cinch'
+require 'cinch/logger'
 
 # Load the bot config 
-config = YAML::load(File.open("config/#{ARGV[0] || 'bot'}.yml"))
+conf = YAML::load(File.open("config/#{ARGV[0] || 'bot'}.yml"))
 
 # Setup the cooldown if one is configured
-$cooldown = { :config => config['cooldowns'] } if config['cooldowns']
+$cooldown = { :config => conf['cooldowns'] } if conf.key?('cooldowns')
 
 # Load Libs
 Dir[File.join('.', 'lib', '*.rb')].each { |file| require file }
@@ -15,27 +18,40 @@ Dir[File.join('.', 'lib', '*.rb')].each { |file| require file }
 # Load Plugins
 Dir[File.join('.', 'plugins', '*.rb')].each { |file| require file }
 
-
+# Init Bot
 @bot = Cinch::Bot.new do
   configure do |c|
-    c.nick         = config['nick']
-    c.server       = config['server']
-    c.channels     = config['chans']
+    # Base Config
+    c.nick         = conf['nick']
+    c.server       = conf['server']
+    c.channels     = conf['chans'].map { |chan| '#' + chan } 
     c.max_messages = 1
 
     # Plugins 
     c.plugins.prefix  = '.'
-    c.plugins.plugins = config['plugins'].map { |plugin| Kernel.const_get(plugin) }
+    c.plugins.plugins = conf['plugins'].map { |plugin| Kernel.const_get(plugin) }
     
     # Link logger config 
-    if config['links']
-      c.plugins.options[LinkLogger][:whitelist]   = config['links']['whitelist']
-      c.plugins.options[LinkLogger][:reportstats] = config['links']['reportstats']
+    if conf.key?('links')
+      c.plugins.options[LinkLogger][:logonly]     = conf['links']['logonly']
+      c.plugins.options[LinkLogger][:whitelist]   = conf['links']['whitelist']
+      c.plugins.options[LinkLogger][:reportstats] = conf['links']['reportstats']
     end 
 
-    # 
-  
+    # Tumblr config 
+    if conf.key?('tumblr')
+      c.plugins.options[LinkLogger][:tumblr] = { :username => conf['tumblr']['username'], 
+                                                 :password => conf['tumblr']['password'],  
+                                                 :group    => conf['tumblr']['group'] } 
+    end
   end
 end
 
+# Loggers
+if conf.key?('logging')
+  conf['logging'].each do |channel| 
+    @bot.loggers << Cinch::Logger::CanonicalLogger.new(channel, conf['nick'])
+  end
+end
+   
 @bot.start
--- plugins/link_logger.rb
+++ plugins/link_logger.rb
@@ -4,6 +4,7 @@ class LinkLogger
   require 'twitter'
   require 'nokogiri'
   require 'open-uri'
+  require 'tumblr' 
 
   listen_to :channel
   self.help = 'Use .links to see the last 10 links users have posted to the channel.'
@@ -29,16 +30,14 @@ class LinkLogger
 
   def listen(m)
     urls = URI.extract(m.message, ["http", "https"])
-
-
     urls.each do |url|
       @storage.data[:history][m.channel.name] ||= Hash.new 
   
       if @storage.data[:history][m.channel.name].key?(url)
         link = @storage.data[:history][m.channel.name][url]
         
-        if whitelisted?(url)
-          m.reply("#{link[:short_url]} ∴  #{link[:title]}") unless link[:title].nil? 
+        if spam_channel?(url)
+          m.reply("#{link[:short_url]} ∴  #{link[:title]}") unless link[:title].nil?  
 
           unless config[:reportstats] == false
             if link[:count] == 1
@@ -52,18 +51,20 @@ class LinkLogger
         @storage.data[:history][m.channel.name][url][:count] += 1
       # Twitter Statuses
       elsif 
-        if tweet = url.match(/https?:\/\/mobile|w{3}?\.?twitter\.com\/?#?!?\/([^\/]+)\/statuse?s?\/(\d+)\/?/)
-          status = Twitter.status(tweet[2]).text
-          m.reply "@#{tweet[1]} tweeted \"#{status}\"."
-        elsif tweet = url.match(/https?:\/\/mobile|w{3}?\.?twitter\.com\/?#?!?\/([^\/]+)/)
-          m.reply "http://twitter.com/#{tweet[1]} ∴ #{tweet[1]} on Twitter"
+        if spam_channel?(url)  
+          if tweet = url.match(/https?:\/\/mobile|w{3}?\.?twitter\.com\/?#?!?\/([^\/]+)\/statuse?s?\/(\d+)\/?/)
+            status = Twitter.status(tweet[2]).text
+            m.reply "@#{tweet[1]} tweeted \"#{status}\"."
+          elsif tweet = url.match(/https?:\/\/mobile|w{3}?\.?twitter\.com\/?#?!?\/([^\/]+)/)
+            m.reply "http://twitter.com/#{tweet[1]} ∴ #{tweet[1]} on Twitter"
+          end
         end
       else   
         short_url = shorten(url)
         title = get_title(url)
         
         # Only spam the channel if you have a title and url.
-        if whitelisted?(url)
+        if spam_channel?(url)
           m.reply("#{short_url} ∴  #{title}") if short_url && title 
         end
 
@@ -84,6 +85,10 @@ class LinkLogger
 
   private
 
+  def spam_channel?(url) 
+    whitelisted?(url) && !logonly?
+  end
+
   def whitelisted?(url)
     return true unless config[:whitelist]
 
@@ -99,21 +104,41 @@ class LinkLogger
 
     # If the link is to an image, extract the filename.
     if url.match(/\.jpg|gif|png$/)
+      post_image(url) if config[:tumblr]
+      
       # unless it's from reddit, then change the url to the gallery to get the image's caption.
       if url.match(/^https?:\/\/i\.imgur\.com/)
         imgur_id = url.match(/([A-Za-z0-9]{5})\.jpg|gif|png$/)[1]
-        debug "IMGUR: #{imgur_id}"
         url = "http://imgur.com/#{imgur_id}"
       else 
         return "Image: #{url.match(/\/([^\/]+\.jpg|gif|png)$/)[1]}"
       end
     end
 
-    debug url 
-
-    
     # Grab the element, return nothing if  the site doesn't have a title.
     page = Nokogiri::HTML(open(url)).css('title')
     return page.first.content.strip.gsub(/\s+/, ' ') unless page.empty?
   end
+
+  def post_image(url)
+    document = YAML::dump({'type' => 'regular', 'group' => config[:tumblr][:group]})
+    document << "---\n"
+    document << "<p><img src='#{url}' width='500'><br/><a href='#{url}'>#{url}</a></p>"
+    puts document
+    request = Tumblr.new(config[:tumblr][:username], config[:tumblr][:password]).post(document)
+    request.perform do |response|
+      if response.success?
+        debug "Success"
+      else
+        debug "Something went wrong  #{response.code} #{response.message}"
+      end
+    end
+  end
+
+  def logonly? 
+    return false if config[:logonly].nil?
+    return true  if config[:logonly] == true
+    false 
+  end
+
 end
