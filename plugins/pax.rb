dda9f6bfd150ff22238141ee77730e20e40f66a6
Brian Haberer <bhaberer@gmail.com>
2012-06-26 18:17:05 -0700

    Updating paxtimer to support both paxes

--- plugins/pax.rb
+++ plugins/pax.rb
@@ -3,16 +3,30 @@ class PaxTimer
   require 'time-lord'
   cooldown
 
-  match /time|pax|timetillpax/
+  match /(time|pax|timetillpax)\z/, method: :next_pax
+  match /east|paxeast/, method: :east
+  match /prime|paxprime/, method: :prime
+
 
   def initialize(*args) 
     super 
-    @pax = { :name => 'PAX Prime', :time => Time.local(2012, 8, 31, 7) } 
+    @pax = { :prime => Time.local(2012, 8, 31, 7),
+             :east  => Time.local(2013, 3, 22, 4) }
   end 
 
-  def execute(m)
-    days = (@pax[:time] - Time.now) / (60 * 60 * 24)
+  def next_pax(m)
+    @pax[:east] < @pax[:prime] ? east(m) : prime(m)
+  end
+
+  def east(m)
+    days = (@pax[:east] - Time.now) / (60 * 60 * 24)
     hours = (days - days.floor) * 24 
-    m.reply "#{@pax[:name]} is #{days.floor} days#{hours > 1 ? ", #{hours.floor} hours " : ''}away."
+    m.reply "PAX East is #{days.floor} days#{hours > 1 ? ", #{hours.floor} hours " : ''}away."
+  end
+
+  def prime(m)
+    days = (@pax[:prime] - Time.now) / (60 * 60 * 24)
+    hours = (days - days.floor) * 24 
+    m.reply "PAX Prime is #{days.floor} days#{hours > 1 ? ", #{hours.floor} hours " : ''}away."
   end
 end
