6e607c0a1a368400b8c18a24a904f97d5da639bf
Brian Haberer <bhaberer@gmail.com>
2012-10-08 11:59:12 -0700

    Add some more sanity to search

--- plugins/log_search.rb
+++ plugins/log_search.rb
@@ -5,14 +5,25 @@ class LogSearch
   match /search (.*)/
 
   def execute(m, search)
+    # Don't listen to searches in channels. Reduce SPAM. 
+    return if m.channel?
+
+    # However, make sure the user is in a channel with the bot
+    chans = @bot.channels
+    chans.delete_if { |chan| !chan.has_user?(m.user) } 
+    return if chans.empty?
+
+    @max_results = config[:search_max] || 5
     @matches = []  
+    
+    # Search the logs for the phrase, this is pretty simple.  
     Dir[File.join('.', 'logs', "*.log")].reverse.each do |file| 
       @matches += File.open(file, 'r').grep(Regexp.new(search))
-      break if @matches.length > 10
+      # For the sake of sanity, stop looking once we find @max_results
+      break if @matches.length > @max_results
     end
-  
-    debug m.message
-
+ 
+    # I hate new lines.  
     @matches.map! { |msg| msg.chomp } 
 
     if @matches.empty?
@@ -20,11 +31,11 @@ class LogSearch
     else
       msg = []
       msg << "Found #{@matches.count} matches"
-      msg << "before giving up." if @matches.length > 10
-      msg << "Here's the last 10:"
+      msg << "before giving up." if @matches.length > @max_results
+      msg << "Here's the last #{@max_results}:"
       m.user.msg msg.join(' ')
-      @matches[-10..-1].each do |msg| 
-        m.user.msg msg
+      @matches[-@max_results..-1].each do |match| 
+        m.user.msg match
       end
     end
   end
